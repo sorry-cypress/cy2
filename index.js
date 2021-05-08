e5409406073064b7e00e50e19aff5a0662bf8324
Andrew Goldis <agoldis@gmail.com>
2021-05-08 00:23:29 -0700

    feat: export run and patch from index
    Closes #4.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const patch = require('./lib/patch');
+const lib = require('./lib/patch');
 
 exports.patch = async (cypressAPIUrl) => {
-  await patch.patch(cypressAPIUrl, await patch.getCypressCLIBinPath());
+  await lib.patch(cypressAPIUrl, await lib.getCypressCLIBinPath());
+};
+
+exports.run = async (cypressAPIUrl, label = 'cy2') => {
+  const cliBinPath = await lib.getCypressCLIBinPath();
+  lib.patch(cypressAPIUrl, cliBinPath);
+  console.log(`[${label}] Launching cypress from ${cliBinPath}`);
+  console.log(`[${label}] Cypress API URL: ${cypressAPIUrl}`);
+  lib.run(cliBinPath);
 };
