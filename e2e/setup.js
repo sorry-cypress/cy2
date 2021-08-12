fbebe112c022a4a02e80419250d3365931e9931f
Andrew Goldis <agoldis@gmail.com>
2021-08-11 23:27:01 -0700

    chore: fix tests

--- e2e/setup.js
+++ e2e/setup.js
@@ -3,11 +3,12 @@ const shell = require('shelljs');
 
 exports.installCmd = {
   local: 'npm install --save-dev cypress',
-  global: 'npm install -g --force --prefix=$HOME/.local cypress',
+  global: 'npm install -g --force --prefix=~/.local cypress',
 };
 exports.runE2E = (dirname, installCmd) => {
   shell.rm('-rf', dirname);
   shell.mkdir('-p', dirname);
+  shell.mkdir('-p', '~/.local/lib');
   shell.cp('./test.js', dirname);
   shell.cd(dirname);
   shell.exec('npm init -y');
