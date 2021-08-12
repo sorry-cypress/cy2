5fc945bfd48f5d65dad86ef9bcde51ec6ea3c1ca
Andrew Goldis <agoldis@users.noreply.github.com>
2021-08-12 14:45:43 -0700

    chore: fix tests (#11)
    * chore: fix tests
    
    * add unit tests for cci
    
    * chore: try __dirname
    
    * add rdp
    
    * fix global install e2e test
    
    * reenable rdp
    
    * use process.pwd()
    
    * e2e - use different install for windown and linux
    
    * Add status badges

--- e2e/setup.js
+++ e2e/setup.js
@@ -3,11 +3,13 @@ const shell = require('shelljs');
 
 exports.installCmd = {
   local: 'npm install --save-dev cypress',
-  global: 'npm install -g --force --prefix=$HOME/.local cypress',
+  globalWindows: 'npm install -g cypress',
+  globalLinux: 'npm install -g --force --prefix=~/.local cypress',
 };
 exports.runE2E = (dirname, installCmd) => {
   shell.rm('-rf', dirname);
   shell.mkdir('-p', dirname);
+  shell.mkdir('-p', '~/.local/lib');
   shell.cp('./test.js', dirname);
   shell.cd(dirname);
   shell.exec('npm init -y');
