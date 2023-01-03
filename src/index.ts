dc70ce253f12f022f0a9775c543e6f58bd2e82ba
Andrew Goldis <agoldis@gmail.com>
2023-01-03 01:26:19 -0800

    fix: change the license

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
+/*! 
+cy2 - use cypress runner with an custom API URLs
+Copyright (C) 2023 Andrew Goldis (agoldis@gmail.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import 'source-map-support/register';
 
 export { getCypressCLIBinPath } from './bin-path';
