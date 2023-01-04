d2a9df0d4393596c9581ef50662c6cd323804d5a
Andrew Goldis <agoldis@gmail.com>
2023-01-03 16:56:45 -0800

    chore: add global test

--- e2e/test.js
+++ e2e/test.js
@@ -2,13 +2,23 @@ const { run } = require('cy2');
 console.log(require.resolve.paths('cy2'));
 process.env.CYPRESS_API_URL = 'https://myURIhere.com';
 
+/* 
+
+The test is supposed to be run with NODE_PATH being set:
+
+- to the pwd of the project for locally installed cypress
+- to "`npm prefix -g`/lib/node_modules` for globally installed cypress
+
+See the .circle/config.yml file for an example of how to set this up. Note the use of npm_config_prefix to set the location for global npm installs.
+*/
+
 /*
  https://docs.npmjs.com/cli/v8/configuring-npm/folders#node-modules
 
  Global installs on Unix systems go to {prefix}/lib/node_modules. Global installs on Windows go to {prefix}/node_modules (that is, no lib folder.)
+
  
 */
-
 async function main() {
   console.log('Running cypress tests via "run" API');
 
