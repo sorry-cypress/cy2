12140cde58df9bea079950ce03ae69ca83dc34c8
Andrew Goldis <agoldis@users.noreply.github.com>
2023-01-09 23:06:51 +0000

    fix: teminate proxy (#53)

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,3 @@
-// jest.config.js
-// const fs = require('node:fs');
-
-// const swcrc = JSON.parse(fs.readFileSync('.swcrc', 'utf8'));
-
-// // If you have other plugins, change this line.
-// ((swcrc.jsc ??= {}).experimental ??= {}).plugins = [['jest_workaround', {}]];
-
 module.exports = {
   clearMocks: true,
   testMatch: ['**/*.spec.ts'],
